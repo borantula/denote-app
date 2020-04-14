8c973ca3bb7b014836b01408ddc0c9fcd5d4ab47
Bora Yalcin <b.yalcin@catawiki.nl>
2020-04-14 10:49:45 +0200

    favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,25 @@ import DateBar from "../components/DateBar";
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>Denote</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest"></link>
       <link
         href="https://fonts.googleapis.com/css2?family=Neucha&display=swap"
         rel="stylesheet"
